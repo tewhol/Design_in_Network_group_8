5b6432a15871fd2eb14264b5f2004626618f309d
allis103 <a.melli@student.tudelft.nl>
2024-02-29 15:56:32 +0100

    addwd  the r

---
+++ Network_assignment.R
@@ -0,0 +1,19 @@
+library(“statnet”)
+library(“sna”)
+
+
+network_data <- read.csv("FILENAME.csv", header=F)
+network_data_sociomatrix <- network(network_data, directed=FALSE)
+network_data_sociomatrix
+
+attributes <- read.csv("company_attribute_data.csv")
+set.vertex.attribute(network_data_sociomatrix,names(attributes), 
+                     attributes)
+list.vertex.attributes(network_data_sociomatrix)
+edge <- as.matrix(read.csv("company_edge_closeness.csv", header=F))
+network_data_sociomatrix<-
+  as.network(edge,directed=FALSE,ignore.eval=FALSE,names.eval="closeness")
+
+summary(network_data_sociomatrix %e% "closeness")
+get.edge.attribute(network_data_sociomatrix,"closeness")
+
